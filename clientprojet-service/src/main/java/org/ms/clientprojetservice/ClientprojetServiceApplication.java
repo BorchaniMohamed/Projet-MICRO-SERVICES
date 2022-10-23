fe6c04a7c75019415bd3c67cbaaca20506189f23
medborchani <med@yahoo.fr>
2022-10-23 18:03:27 +0100

    Add Client-Service

---
+++ clientprojet-service/src/main/java/org/ms/clientprojetservice/ClientprojetServiceApplication.java
@@ -0,0 +1,13 @@
+package org.ms.clientprojetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientprojetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientprojetServiceApplication.class, args);
+	}
+
+}
---
+++ clientprojet-service/src/test/java/org/ms/clientprojetservice/ClientprojetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ms.clientprojetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientprojetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
