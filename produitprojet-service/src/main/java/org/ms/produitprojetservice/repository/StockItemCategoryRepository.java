4a606065c5d42f0b137746003ab2adac703eab42
medborchani <med@yahoo.fr>
2022-10-27 20:40:06 +0100

    update change RESTCONTROLLER to REPOSITORYRESTCONTROLLER project

--- produitprojet-service/src/main/java/org/ms/produitprojetservice/repository/StockItemCategoryRepository.java
+++ produitprojet-service/src/main/java/org/ms/produitprojetservice/repository/StockItemCategoryRepository.java
@@ -2,7 +2,7 @@ package org.ms.produitprojetservice.repository;
 
 import org.ms.produitprojetservice.entities.StockItemCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RestController;
-@RestController //TODO: modifer to @RepositoryRestController
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+@RepositoryRestController
 public interface StockItemCategoryRepository extends JpaRepository<StockItemCategory, Long> {
 }
