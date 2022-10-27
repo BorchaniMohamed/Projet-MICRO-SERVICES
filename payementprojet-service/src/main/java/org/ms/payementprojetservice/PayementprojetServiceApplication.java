891d9119bd36a17fe5546512dd411c50b5acc3fe
medborchani <med@yahoo.fr>
2022-10-27 19:57:20 +0100

    add payement project

---
+++ payementprojet-service/src/main/java/org/ms/payementprojetservice/PayementprojetServiceApplication.java
@@ -0,0 +1,13 @@
+package org.ms.payementprojetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayementprojetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayementprojetServiceApplication.class, args);
+	}
+
+}
---
+++ payementprojet-service/src/test/java/org/ms/payementprojetservice/PayementprojetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ms.payementprojetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayementprojetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
