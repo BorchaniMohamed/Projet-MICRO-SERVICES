d3c060bd20e3feeeab552b6203b08923f7a6c0a8
medborchani <med@yahoo.fr>
2022-10-31 14:42:03 +0100

    update data.sql clientservice

--- clientprojet-service/src/main/resources/data.sql
+++ clientprojet-service/src/main/resources/data.sql
@@ -1,6 +1,8 @@
-
-INSERT INTO Customer (name,delivreyadress,phone,city,mail) values ('Ali','Ain',22661936,'SFAX','ali@yahoo.fr');
-INSERT INTO Customer (name,delivreyadress,phone,city,mail) values ('med','Akouda',44556677,'Sousse','med@yahoo.fr');
+INSERT INTO customercategory(name) values('particulier');
+INSERT INTO customercategory(name) values('entreprise');
+INSERT INTO Customer (name,delivreyadress,phone,city,mail,customer_category_id) values ('Ali','Ain',22661936,'SFAX','ali@yahoo.fr',1);
+INSERT INTO Customer (name,delivreyadress,phone,city,mail,customer_category_id) values ('med','Akouda',44556677,'Sousse','med@yahoo.fr',2);
+INSERT INTO Customer (name,delivreyadress,phone,city,mail,customer_category_id) values ('ahmed','Ariana',71234566,'Tunis','ahmed@yahoo.fr',2);
 
 
 
